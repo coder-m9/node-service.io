1894d16a5f48a1e077eae75ba205a471fc0f432d
coder-m9 <35654636+coder-m9@users.noreply.github.com>
2018-10-06 10:20:37 +0530

    Create cluster.js

---
+++ bin/cluster.js
@@ -0,0 +1,31 @@
+
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+
+// To increase the threadpool
+//process.env.UV_THREADPOOL_SIZE=32
+
+if (cluster.isMaster) {
+
+    cluster.setupMaster({
+        exec: 'bin/www'
+    });
+
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('listening', function (worker, address) {
+        console.log('Worker id: ' + worker.id + ' listening at: ' + JSON.stringify(address));
+    });
+
+    Object.keys(cluster.workers).forEach(function (id) {
+        console.log('Worker id: ' + id + ' with pid: ' + cluster.workers[id].process.pid);
+    });
+
+    cluster.on('exit', function (worker, code, signal) {
+        console.log('worker ' + worker.process.pid + ' died: Respawning...');
+        cluster.fork();
+    });
+}
